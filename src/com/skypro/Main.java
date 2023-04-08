303c24f6d85ae907e3db1a7b92e8f94694b15f8f
maks <maxim_86nva@mail.ru>
2023-04-08 11:10:41 +0300

    решение дз

--- src/com/skypro/Main.java
+++ src/com/skypro/Main.java
@@ -1,28 +1,64 @@
 package com.skypro;
 
-import java.util.Locale;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        //1
-        int year = 2021;
-        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        //String - 1
+        String fullName = String.format("ФИО сотрудника — %s %s %s", lastName, firstName, middleName);
+        System.out.println(fullName);
+
+        //String - 2
+        String fullNameUpperCase = String.format(
+                "ФИО сотрудника — %s %s %s",
+                lastName.toUpperCase(),
+                firstName.toUpperCase(),
+                middleName.toUpperCase()
+        );
+        System.out.println(fullNameUpperCase);
+
+        //String - 3
+        String input = "ёж";
+        String replaceSymbol = input.replace('ё', 'e');
+        System.out.println(replaceSymbol);
+
+
+        //Methods - 1
+        checkYearIsLeap(2020);
+
+        //Methods - 2
+        getOsRecommendation(2018, 0);
+        getOsRecommendation(2010, 1);
+
+        //Methods - 3
+        task3(2);
+        task3(30);
+        task3(80);
+        task3(130);
+    }
+
+    public static void checkYearIsLeap(int year) {
+        if (isLeapYear(year)) {
             System.out.println(year + " - високосный год");
         } else {
             System.out.println(year + " - не високосный год");
         }
+    }
 
+    public static boolean isLeapYear(int year) {
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+    }
 
-        //2
-        int clientDeviceYear = 2016;
-        int clientOS = 0;
+    public static void getOsRecommendation(int currentYear, int clientOs) {
         int boarderYear = 2015;
 
         String lightInfo;
 
-        if (clientDeviceYear < boarderYear) {
+        if (currentYear < boarderYear) {
             lightInfo = "облегченную ";
         } else {
             lightInfo = "";
@@ -30,7 +66,7 @@ public class Main {
 
         String osType;
 
-        if (clientOS == 0) {
+        if (clientOs == 0) {
             osType = "iOS";
         } else {
             osType = "Android";
@@ -38,20 +74,25 @@ public class Main {
 
         String generalInfo = "Установите " + lightInfo + "версию приложения для " + osType + " по ссылке";
         System.out.println(generalInfo);
+    }
 
+    public static void task3(int distance) {
+        int days = computeDeliveryDays(distance);
 
-        //3
-        int days;
-        int distance = 25;
+        String deliveryInfo = days == -1 ? "Доставки нет" : String.format("Потребуется дней: %s", days);
 
+        System.out.println(deliveryInfo);
+    }
+
+    public static int computeDeliveryDays(int distance) {
         if (distance <= 20) {
-            days = 1;
-        } else if (distance > 20 && distance <= 60) {
-            days = 2;
-        } else if (distance > 60 && distance <= 100) {
-            days = 3;
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else if (distance <= 100) {
+            return 3;
         } else {
-            days = -1;
+            return -1;
         }
     }
 }
